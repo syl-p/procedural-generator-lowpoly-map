e378b24f73774e4a0df27cd27e5a49a4fcaa7871
Sylvain Pastor <sylvain@mbpdeatnosadmin.home>
2023-12-04 14:02:10 +0100

    fix: update building conf

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/procedural-generator-lowpoly-map'
+    base: '/procedural-generator-lowpoly-map',
+    build: {
+        target: 'esnext'
+    }
 })
